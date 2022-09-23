b28ffe12fef113aaf42720d1f13126826c734b76
OrionFE <102513015+OrionFE@users.noreply.github.com>
2022-09-23 19:21:38 +0200

    display recipes

--- app.js
+++ app.js
@@ -11,4 +11,82 @@ async function getRecipes() {
     });
 }
 
-getRecipes();
+function displayRecipe() {
+  const containerRecipe = document.querySelector(".recipe-container");
+  console.log(containerRecipe);
+  recipes.forEach((recipe) => {
+    const { name, time, ingredients, description } = recipe;
+
+    const card = document.createElement("div");
+    card.classList.add("card");
+    const img = document.createElement("div");
+    img.classList.add("img-to-display");
+    card.appendChild(img);
+    const headingCard = document.createElement("div");
+    headingCard.classList.add("heading-card");
+    card.appendChild(headingCard);
+    const title = document.createElement("h3");
+    title.innerText = name;
+    const timer = document.createElement("div");
+    timer.classList.add("timer");
+    timer.innerHTML = `
+    <i class="fa-regular fa-clock"></i>
+    ${time} min
+    `;
+
+    headingCard.appendChild(title);
+    headingCard.appendChild(timer);
+    const listIngredient = document.createElement("ul");
+
+    ingredients.forEach((item) => {
+      const { ingredient, quantity, unit } = item;
+      let prefixUnit;
+
+      const list = document.createElement("li");
+
+      if (unit == "ml") {
+        prefixUnit = "ml";
+      } else if (unit == "grammes") {
+        prefixUnit = "g";
+      } else if (unit == "cl") {
+        prefixUnit = "cl";
+      } else if (unit == "tasse") {
+        prefixUnit = "tasse";
+      } else {
+        prefixUnit = "";
+      }
+
+      if (quantity == null) {
+        list.innerHTML = `<span>${ingredient}</span>`;
+      } else {
+        list.innerHTML = `<span>${ingredient}:</span> ${quantity}${prefixUnit}`;
+      }
+
+      listIngredient.appendChild(list);
+    });
+
+    const tuto = document.createElement("div");
+    tuto.classList.add("tuto");
+    card.appendChild(tuto);
+
+    tuto.appendChild(listIngredient);
+
+    const prepContainer = document.createElement("div");
+    prepContainer.classList.add("prep-container");
+
+    const prep = document.createElement("p");
+    prep.classList.add("prep");
+    prep.innerText = description;
+    prepContainer.appendChild(prep);
+    tuto.appendChild(prepContainer);
+
+    containerRecipe.appendChild(card);
+  });
+}
+
+async function init() {
+  await getRecipes();
+  displayRecipe();
+}
+
+init();
